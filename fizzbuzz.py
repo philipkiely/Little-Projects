02b649084b85fa70f79b5d3c2d4a7328d8216708
Philip Kiely <kielyphi@grinnell.edu>
2017-02-10 10:39:05 -0600

    Fixed FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
-for x in range (1, 50):
-    if x % 3 == 0:
+for x in range (-22, 22):
+    if x % 3 == 0 and x % 5 == 0:
+        print("Fizz Buzz")
+    elif x % 3 == 0:
         print ("Fizz")
-    if x % 5 == 0:
+    elif x % 5 == 0:
         print ("Buzz")
     else:
         print (x)
